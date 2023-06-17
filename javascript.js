9ed22dba98ceba6a9396bc509d5d3dffbe1080e3
Ameh Sunday <Ameh.S@bullseye.local>
2023-06-17 12:18:23 +0100

    last commit

--- javascript.js
+++ javascript.js
@@ -1,15 +1,25 @@
-const handle = document.getElementById('handle');
-const checkbox = document.getElementById('toggle');
 
-  handle.addEventListener('click', function() {
-    checkbox.checked = !checkbox.checked;
-    handle.classList.toggle('translate-x-full');
+  const price = document.getElementById('price'); 
+  const planleft = document.getElementById('planleft');
+  const planmiddle = document.getElementById('planmiddle');
+  const planright = document.getElementById('planright');
+  const tog = document.getElementById('button-tog');
 
-    if (checkbox.checked) {
-      // Code to handle the monthly option being selected
-      console.log('Monthly selected');
-    } else {
-      // Code to handle the annually option being selected
-      console.log('Annually selected');
-    }
-  });
+    
+  flag = true
+  
+  butt.addEventListener('click', ()=>{
+    toggle()
+  })
+  
+  const toggle = ()=>{
+    tog.className = flag ? "block w-4 h-4 bg-white rounded-full shadow-md transform duration-200 ease-in-out absolute left-1 top-1" : "block w-4 h-4 bg-white rounded-full shadow-md transform duration-200 ease-in-out absolute right-1 top-1"
+  
+    planleft.innerHTML = flag ? "199.99" : "19.99"
+    planmiddle.innerHTML = flag ? "249.99" : "24.99"
+    planright.innerHTML = flag ? "399.99" : "39.99"
+  
+  
+    flag = !flag
+  }
+  
